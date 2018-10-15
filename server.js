d0ddc5159dd571a9d75133b5c9d27383e76e9a10
Ashley <ashley@Jamess-MacBook-Air.local>
2018-10-15 12:56:27 -0500

    added all views

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ require('.db/db');
 const groceryController = require('./controllers/grocery');
 // const itemsController = require('./controllers/items');
 
+//Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+//Controller
+app.use('/grocery', groceryController);
+
+//Index
+app.get('/', (req, res) => {
+	res.render('index.ejs');
+})
+
+
+//listen
+app.use('')
 app.listen(3000, () => {
 	console.log(listening);
 })
