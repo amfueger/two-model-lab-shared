80c7e334a6f940559c7ac21a84e84458e6647fef
Ashley <ashley@Jamess-MacBook-Air.local>
2018-10-15 13:00:15 -0500

    fixing up new and index in grocery

---
+++ controllers/grocery.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const Grocery = require('..models/grocery');
+
+
+router.get('/new', (req, res) => {
+	res.render('/grocery/new.ejs')
+})
+
+
+
+
+
+
+
+
+module.exports = router;
